c0efa44ade54b5e563b4b270299fc88f34b578b9
Frede94 <Frede94@users.noreply.github.com>
2018-02-09 10:20:08 +0100

    ...

--- src/attendance/automation/AttendanceAutomation.java
+++ src/attendance/automation/AttendanceAutomation.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
