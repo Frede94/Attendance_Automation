f61a40be16720cfedceaa5fb290239741b163b89
Frede94 <Frede94@users.noreply.github.com>
2018-02-21 10:35:59 +0100

    Correct window
    Correct window næsten færdigt, mangler nogle mock data

--- src/attendance/automation/gui/RootPrototypeController.java
+++ src/attendance/automation/gui/RootPrototypeController.java
@@ -9,16 +9,27 @@ import attendance.automation.AttendanceAutomation;
 import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 /**
  * FXML Controller class
@@ -108,25 +119,83 @@ public class RootPrototypeController implements Initializable
     @FXML
     private void correctAction(ActionEvent event)
     {
+        // Create the custom dialog.
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Teacher Login");
+        dialog.setHeaderText(" ");
 
-        try
+// Set the icon (must be included in the project).
+        dialog.setGraphic(new ImageView(this.getClass().getResource("Images/logo2.PNG").toString()));
+// Set the button types.
+        ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+// Create the username and password labels and fields.
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField username = new TextField();
+        username.setPromptText("Username");
+        PasswordField password = new PasswordField();
+        password.setPromptText("Password");
+
+        grid.add(new Label("Username:"), 0, 0);
+        grid.add(username, 1, 0);
+        grid.add(new Label("Password:"), 0, 1);
+        grid.add(password, 1, 1);
+
+// Enable/Disable login button depending on whether a username was entered.
+        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+        loginButton.setDisable(true);
+
+// Do some validation (using the Java 8 lambda syntax).
+        username.textProperty().addListener((observable, oldValue, newValue) ->
         {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(AttendanceAutomation.class.getResource("gui/CorrectWindow.fxml"));
-            BorderPane chartOverview = (BorderPane) loader.load();
+            loginButton.setDisable(newValue.trim().isEmpty());
+        });
 
-            // Set person overview into the center of root layout.
-            borderRoot.setCenter(chartOverview);
+        dialog.getDialogPane().setContent(grid);
 
-            // Give the controller access to the main app.
-            ChartViewController controller = loader.getController();
+// Request focus on the username field by default.
+        Platform.runLater(() -> username.requestFocus());
+
+// Convert the result to a username-password-pair when the login button is clicked.
+        dialog.setResultConverter(dialogButton ->
+        {
+            if (dialogButton == loginButtonType)
+            {
+                try
+                {
+                    // Load person overview.
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(AttendanceAutomation.class.getResource("gui/CorrectWindow.fxml"));
+                    BorderPane chartOverview = (BorderPane) loader.load();
+
+                    // Set person overview into the center of root layout.
+                    borderRoot.setCenter(chartOverview);
+
+                    // Give the controller access to the main app.
+                    ChartViewController controller = loader.getController();
 //            controller.setMainApp(mainApp);
 
-        } catch (IOException e)
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                return new Pair<>(username.getText(), password.getText());
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+
+        result.ifPresent(usernamePassword ->
         {
-            e.printStackTrace();
-        }
+            System.out.println("Username=" + usernamePassword.getKey() + ", Password=" + usernamePassword.getValue());
+        });
+
     }
 
     @FXML
