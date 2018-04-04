4e40c6ed6a1196276838815ca0da45303e639e48
Mikkel K <mikk6578@easv365.dk>
2018-04-04 13:00:34 +0200

    Person BE
    En BE for personer

---
+++ src/attendance/automation/be/Person.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance.automation.be;
+
+/**
+ *
+ * @author Mikkel
+ */
+public class Person
+{
+
+    private int Id;
+    private String Lname;
+    private String Fname;
+    private String Email;
+    private String Address;
+    private int ZipCode;
+
+    public int getId()
+    {
+        return Id;
+    }
+
+    public void setId(int Id)
+    {
+        this.Id = Id;
+    }
+
+    public String getLname()
+    {
+        return Lname;
+    }
+
+    public void setLname(String Lname)
+    {
+        this.Lname = Lname;
+    }
+
+    public String getFname()
+    {
+        return Fname;
+    }
+
+    public void setFname(String Fname)
+    {
+        this.Fname = Fname;
+    }
+
+    public String getEmail()
+    {
+        return Email;
+    }
+
+    public void setEmail(String Email)
+    {
+        this.Email = Email;
+    }
+
+    public String getAddress()
+    {
+        return Address;
+    }
+
+    public void setAddress(String Address)
+    {
+        this.Address = Address;
+    }
+
+    public int getZipCode()
+    {
+        return ZipCode;
+    }
+
+    public void setZipCode(int ZipCode)
+    {
+        this.ZipCode = ZipCode;
+    }
+
+}
